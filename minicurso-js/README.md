7ab615c1ad43199f97f56946aacf3b9070791525
carol junqueira <junqueiracarolina@live.com>
2016-09-23 16:18:39 -0300

    primeiro commit

---
+++ minicurso-js/README.md
@@ -0,0 +1,3 @@
+# Site desenvolvido para o Minicurso de Javascript
+
+Site exemplo, desenvolvido como exemplo para i Minicurso de Javascript para mulheres.
---
+++ minicurso-js/js/sample_code.js
@@ -0,0 +1,8 @@
+//função para movimentar o gato 200 pixels para a esquerda
+// com velocidade de 1000 milisegundos
+$("#resultImage").animate(
+	{
+		left: "200px"		
+	},
+	1000
+);
